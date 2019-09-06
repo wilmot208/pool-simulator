e887adb8a27aa410846aa6d215260dd86b1c36ac
Simon Liu <cyamonide@gmail.com>
2019-09-06 14:42:22 -0400

    Contruct table by pymunk march

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ from pygame.color import *
 # pymunk imports
 import pymunk
 import pymunk.pygame_util
+import pymunk.autogeometry
+from pymunk import Vec2d
 
 from ball import Ball
 from table import Table
@@ -79,15 +81,64 @@ class PoolGame(object):
         Create the static bodies.
         :return: None
         """
-        static_body = self._space.static_body
-        static_lines = Table({"height": TABLE_HEIGHT, "length": TABLE_LENGTH, "corner_pocket_width": CORNER_POCKET_WIDTH,
-                              "central_pocket_width": CENTRAL_POCKET_WIDTH, "corner_pocket_depth": CORNER_POCKET_DEPTH,
-                              "central_pocket_depth": CENTRAL_POCKET_DEPTH, "static_body": static_body})
-        static_lines = static_lines.shift(100, 100)
-        for line in static_lines:
-            line.elasticity = 0.95
-            line.friction = 0.9
-        self._space.add(static_lines)
+        space = self._space
+
+        logo_img = pygame.image.load("table_outline.png")
+        logo_bb = pymunk.BB(0, 0, logo_img.get_width(), logo_img.get_height())
+
+        def sample_func(point):
+            try:
+                p = pymunk.pygame_util.to_pygame(point, logo_img)
+                color = logo_img.get_at(p)
+
+                return color.hsla[2]
+            except:
+                return 0
+
+        line_set = pymunk.autogeometry.PolylineSet()
+
+        def segment_func(v0, v1):
+            line_set.collect_segment(v0, v1)
+
+        logo_img.lock()
+        pymunk.autogeometry.march_soft(
+            logo_bb,
+            logo_img.get_width(), logo_img.get_height(),
+            99,
+            segment_func,
+            sample_func)
+        logo_img.unlock()
+
+        r = 10
+
+        for line in line_set:
+            line = pymunk.autogeometry.simplify_curves(line, .7)
+
+            max_x = 0
+            min_x = 1000
+            max_y = 0
+            min_y = 1000
+            for l in line:
+                max_x = max(max_x, l.x)
+                min_x = min(min_x, l.x)
+                max_y = max(max_y, l.y)
+                min_y = min(min_y, l.y)
+            w, h = max_x - min_x, max_y - min_y
+
+            center = Vec2d(min_x + w/2., min_y + h/2.)
+
+            r += 30
+            if r > 255:
+                r = 0
+
+            if True:
+                for i in range(len(line)-1):
+                    shape = pymunk.Segment(
+                        space.static_body, line[i], line[i+1], 1)
+                    shape.friction = 0.9
+                    shape.elasticity = 0.95
+                    shape.color = (255, 255, 255)
+                    space.add(shape)
 
     def _process_events(self):
         """
